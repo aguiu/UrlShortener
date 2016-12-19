364859a4d360c98b45164b68e413c801417cd1e3
Alvaro Juan Ciriaco <alvarojc95@gmail.com>
2016-12-19 12:51:30 +0100

    async update status - 7 with new tests

--- common/src/test/java/urlshortener/common/web/fixture/ShortURLFixture.java
+++ common/src/test/java/urlshortener/common/web/fixture/ShortURLFixture.java
@@ -1,11 +1,12 @@
 package urlshortener.common.web.fixture;
 
 import urlshortener.common.domain.ShortURL;
+import java.sql.Date;
 
 public class ShortURLFixture {
 
 	public static ShortURL someUrl() {
 		return new ShortURL("someKey", "http://example.com/", null, null, null,
-				null, 307, true, null, null);
+				null, 307, true, null, null, "online", new Date(System.currentTimeMillis()));
 	}
 }
