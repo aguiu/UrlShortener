c45b54c347e4931a4bb42f299ccd3bf12f63d3ce
Eduardo Aguilar <edu_man3@hotmail.com>
2016-12-20 18:35:35 +0100

    first version of the advert upgrade

---
+++ team/src/main/resources/static/js/advert.js
@@ -0,0 +1,16 @@
+function cuenta() {  
+	var interval = window.setInterval(function(msg) {
+		var timeLeft    = $("#timeLeft").html();                                
+		if(eval(timeLeft) == 0){
+			var url = window.location.href;
+		    var array = url.split('/');
+		    var id = array[array.length - 1];
+		    $.getJSON("http://localhost:8080/uri/"+id, function(data){
+		    	window.location= (data.target);   
+		    });
+		        clearInterval(interval);
+		 }else{              
+			 $("#timeLeft").html(eval(timeLeft)- eval(1));
+		 }
+	}, 1000); 
+} 
