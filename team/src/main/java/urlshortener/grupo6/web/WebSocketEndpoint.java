0b57b39c2e03852505f0cf7c61bb873050372b4c
Eduardo Aguilar <edu_man3@hotmail.com>
2017-01-15 14:14:59 +0100

    nueva version de ws

--- team/src/main/java/urlshortener/grupo6/web/WebSocketEndpoint.java
+++ team/src/main/java/urlshortener/grupo6/web/WebSocketEndpoint.java
@@ -7,17 +7,24 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import urlshortener.common.domain.Par;
 import java.lang.Thread;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import urlshortener.common.repository.ShortURLRepository;
 
 @Controller
 public class WebSocketEndpoint{
     private static final Logger logger = LoggerFactory.getLogger(WebSocketEndpoint.class);
+    
+    @Autowired
+	protected ShortURLRepository shortURLRepository;
 
     @MessageMapping("/conected")
     @SendTo("/topic/answer")
     public Integer answeringToWebSocket(){
+    	List<String> allUrl = shortURLRepository.allList();
     	logger.info("accedido al ws endpoint!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     	//Par par = new Par("prueba", 10);
-    	Integer num = new Integer(10);
+    	Integer num =allUrl.size() ;
     	return num;
     }
 
